ac3fadd241f5f9147f402555982f14f19ae3f5c8
Roma Movchan <movchan_roman@list.ru>
2016-03-19 17:31:54 +0300

    hw1-2 fixed

--- hw1-2/task1/js/script.js
+++ hw1-2/task1/js/script.js
@@ -1,8 +1,19 @@
-function Pow(number,power){
+var conNumber = prompt("Введите число");
+var conPower = prompt("Введите степень");
+if ((conNumber != "") && (conPower != "")){
+    if (isNaN(conNumber) || isNaN(conPower) || conPower < 0){
+        alert ('Введеннык вами значения не являются числами или степень меньше нуля , повторите ввод');
+    }else{
+        console.log(conNumber + " в степени " + conPower + " равна: " + pow(conNumber,conPower));
+    }
+}else{
+    alert("Введите данные");
+}
+function pow(number,power){
     var result = 0;
     var newval = number;
     if (power == 0){
-        return result;
+        return 1;
     }else{
         for(var i = 1; i <= power-1; i++){
             result = newval * number;
@@ -10,15 +21,4 @@ function Pow(number,power){
         }
         return result;
     }
-}
-var conNumber = prompt("Введите число");
-var conPower = prompt("Введите степень");
-if ((conNumber != "") && (conPower != "")){
-    if (isNaN(conNumber) || isNaN(conPower) || conPower < 0){
-        alert ('Введеннык вами значения не являются числами или степень меньше нуля , повторите ввод');
-    }else{
-        console.log(conNumber + " в степени " + conPower + " равна: " + Pow(conNumber,conPower));
-    }
-}else{
-    alert("Введите данные");
 }
